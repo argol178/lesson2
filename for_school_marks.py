38c3d6ae7797eca7a6c743eca9d18ad5f6209e8f
Artem Golynin <a.golynin@yandex.ru>
2019-03-05 17:02:41 +0300

    Third commit

--- for_school_marks.py
+++ for_school_marks.py
@@ -1,8 +1,7 @@
 school_marks = [{'school_class': '4a', 'scores': [2,4,4,5,2]},{'school_class': '5b', 'scores': [4,3,5,4,4]},
-                {'school_class': '8v', 'scores': [1,4,3,4,4]}]
+                {'school_class': '8v', 'scores': [1,4,3,4,4]}, {'school_class': '10c', 'scores': [5,2,4,3,3]}]
 
 a = []
-b = []
 
 average_school = 0
 
@@ -14,10 +13,11 @@ for j in a:
 
 print('Средний балл по всей школе составляет {}'.format(average_school/len(a)))
 
-for k in school_marks:
-    b = k['scores']
-    average_class = 0
-    for l in b:
-        average_class += l
 
-    print('Средний балл по классу составляет {}'.format(average_class/len(b)))
+for k in school_marks:
+    average_class = 0
+    for l in k['scores']:
+        c = k['school_class']
+        average_class += l
+    
+    print('Средний балл по {} классу составляет {}'.format(c, average_class/len(k['scores'])))
