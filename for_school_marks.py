d8532609712000cbd84dd5a2aa2908cda885c073
Artem Golynin <a.golynin@yandex.ru>
2019-03-05 20:33:49 +0300

    Fourth commit

--- for_school_marks.py
+++ for_school_marks.py
@@ -1,23 +1,23 @@
-school_marks = [{'school_class': '4a', 'scores': [2,4,4,5,2]},{'school_class': '5b', 'scores': [4,3,5,4,4]},
+school_grades = [{'school_class': '4a', 'scores': [2,4,4,5,2]},{'school_class': '5b', 'scores': [4,3,5,4,4]},
                 {'school_class': '8v', 'scores': [1,4,3,4,4]}, {'school_class': '10c', 'scores': [5,2,4,3,3]}]
 
-a = []
+sum_school_grades = 0
 
-average_school = 0
+for i in school_grades:
+        for j in i['scores']:
+                sum_school_grades += j
 
-for i in school_marks:
-    #if 'scores' in i:
-        a += i['scores']
-for j in a:
-    average_school += j
+avg_school = sum_school_grades/len(school_grades)/len(i['scores'])
 
-print('Средний балл по всей школе составляет {}'.format(average_school/len(a)))
+print('Средний балл по всей школе составляет {}'.format(avg_school))
 
 
-for k in school_marks:
-    average_class = 0
+for k in school_grades:
+    sum_class_grades = 0
     for l in k['scores']:
         c = k['school_class']
-        average_class += l
+        sum_class_grades += l
+
+        avg_grades = sum_class_grades/len(k['scores'])
     
-    print('Средний балл по {} классу составляет {}'.format(c, average_class/len(k['scores'])))
+    print('Средний балл по {} классу составляет {}'.format(k['school_class'], avg_grades))
